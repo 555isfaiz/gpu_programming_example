b3ec281bd0cfc22229702ea54083821134cb4898
Boyuan Xiao <boyuan.xiao@lunatech.nl>
2025-05-29 15:58:09 +0200

    with OpenMP

--- src/code_exam.c
+++ src/code_exam.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
 
-void fill_matrix(int **s, int x_len, int y_len, int draw_start, int draw_end,
+#define DRAW_START 128
+#define DRAW_END 512
+
+void fill_matrix(int **s, 
+                 int x_len, 
+                 int y_len, 
                  int value_to_fill) {
   for (int i = 0; i < y_len; i++) {
     for (int j = 0; j < x_len; j++) {
-      if (i > draw_start && i < draw_end && j > draw_start && j < draw_end) {
+      if (i > DRAW_START 
+                && i < DRAW_END 
+                && j > DRAW_START 
+                && j < DRAW_END) {
+        s[i][j] = value_to_fill;
+      }
+    }
+  }
+}
+
+void fill_matrix_omp(int **s, 
+                     int x_len, 
+                     int y_len, 
+                     int value_to_fill) {
+  #pragma omp parallel for collapse(2)
+  for (int i = 0; i < y_len; i++) {
+    for (int j = 0; j < x_len; j++) {
+      if (i > DRAW_START 
+                && i < DRAW_END 
+                && j > DRAW_START 
+                && j < DRAW_END) {
         s[i][j] = value_to_fill;
       }
     }
@@ -36,8 +62,15 @@ int64_t initialize_matrix(int rows, int cols) {
 }
 
 int main() {
-  int **s = (int **)initialize_matrix(10, 10);
-  fill_matrix(s, 10, 10, 1, 8, 9);
-  print_matrix(s, 10, 10);
+  int **s = (int **)initialize_matrix(32768, 32768);
+  // printf("before: \n");
+  // print_matrix(s, 32768, 32768);
+
+  fill_matrix_omp(s, 32768, 32768, 9);
+
+  // printf("after: \n");
+  // print_matrix(s, 32768, 32768);
   return 0;
 }
+
+
